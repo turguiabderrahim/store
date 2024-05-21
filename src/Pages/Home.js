2260d6bedc1fc163692a1d6443c2ea17246ed6e8
Tourgui Abderrahim <77793447+turguiabderrahim@users.noreply.github.com>
2024-05-21 23:19:30 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
     // const response = await fetch('http://localhost/api/');
     //const response = await fetch('https://fakestoreapi.com/products');
     // const response = await fetch('https://api.escuelajs.co/api/v1/products');
-     const response = await fetch('https://tourgui.000.pe');
+     const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:iDNrOW_s/products');
     const data = await response.json();
     setProducts(data);
   };
