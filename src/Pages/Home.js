6c6cb6116aecf6d44f1f2fc26e2a4ee88f035a07
Tourgui Abderrahim <77793447+turguiabderrahim@users.noreply.github.com>
2024-05-21 23:21:57 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,9 +7,9 @@ export default function Home() {
   const [products, setProducts] = useState([]);
   const fetchData = async () => {
     // const response = await fetch('http://localhost/api/');
-    //const response = await fetch('https://fakestoreapi.com/products');
+    const response = await fetch('https://fakestoreapi.com/products');
     // const response = await fetch('https://api.escuelajs.co/api/v1/products');
-     const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:iDNrOW_s/products');
+     //const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:iDNrOW_s/products');
     const data = await response.json();
     setProducts(data);
   };
